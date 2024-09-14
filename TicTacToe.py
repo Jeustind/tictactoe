19863726162b5fb14093bc70fee939db08c3e1a6
Jeustind <jeustinuc@gmail.com>
2024-09-14 15:42:37 -0600

    Add files via upload

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,47 +1,44 @@
-tablero = [' ' for _ in range]
+tablero = [' ' for _ in range (9)]
 
 def print_tablero():
     fila = ''
     for i in range (9):
-        if (i + 1) %3 == 0:
-            fila += tablero[i] * '\n'
+        if (i + 1) % 3 == 0:
+            fila += f'{tablero[i]}'
             print(fila)
             if i != 8:
-                fila = ''
                 print('_'* 9)
-                fila = ''
-            else:
-                fila += f'{tablero[i]}|'
+            fila = ''  
+        else:
+            fila += f' {tablero[i]} |'
                 
-                def verificar_victoria(jugador):
+def verificar_victoria(jugador):
                     combinaciones = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
                     for c in combinaciones:
-                        if tablero[c(0)] == tablero[c(1)] == tablero[c(2)] == jugador:
+                        if tablero[c[0]] == tablero[c[1]] == tablero[c[2]] == jugador:
                             return True
-                        return False
+                    return False
 
-        def verificar_empate():
+def verificar_empate():
             return ' ' not in tablero
             
-        def jugar(): 
+def jugar(): 
             jugador = 'X'
             while True:
                 print_tablero()
-                print(f'jugador {jugador}')
-                posición = int(input('Ingrese una posición: '))
+                print(f'Es el turno del jugador {jugador}')
+                posición = int(input('Ingrese una posición:')) -1
                 if tablero[posición] == ' ':
-                    tablero[posición] = jugador
-                    if verificar_victoria(jugador):
+                 tablero[posición] = jugador
+                 if verificar_victoria(jugador):
                         print_tablero()
                         print(f'Judador {jugador} ha ganado')
                         break 
-                    if verificar_empate():
+                if verificar_empate():
                         print_tablero()
                         print ('Empate')
                         break
-                    jugador = 'X' if jugador == '0' else '0'
-                else:
-                    print('Posición ocupada')
-
-                    jugador_actual = '0' if jugador_actual == 'X' else 'X'  
-
+                jugador = 'O' if jugador == 'X' else 'X'
+            else:
+                print('Posición ocupada')
+jugar()
